e79fef15b87ed0cea9d94ca5ca5bef64de70a9e8
murphymen <marcin.kochman@gmail.com>
2022-02-12 09:35:55 +0100

    ColorGenerator

--- Assets/Scripts/ColorGenerator.cs
+++ Assets/Scripts/ColorGenerator.cs
@@ -5,44 +5,65 @@ using UnityEngine;
 
 public class ColorGenerator : UnitySingleton<ColorGenerator>
 {
+    public int palletesCount = 1;
     public int paletteSize = 128;
 
     // Dictionary of pair (int, Vector4[64])
-    public Dictionary<int, Color32[]> cellColors;
-
+    public Dictionary<int, uint[]> cellColors;
+    public uint[] palette;
 
     // Start is called before the first frame update
     void Start()
     {
-        cellColors = new Dictionary<int, Color32[]>();
+        cellColors = new Dictionary<int, uint[]>();
 
         // Generate sand palette
         CreateColorPallette(1, new Vector4(0.9f, 0.9f, 0.9f, 1.0f));
-    }
 
-    // Function that converts vector4 to Color32
-    Color32 Vector4ToColor32(Vector4 color)
-    {
-        return new Color32((byte)(color.x * 255), (byte)(color.y * 255), (byte)(color.z * 255), (byte)(color.w * 255));
+        uint[] Palette = new uint[paletteSize]; 
+        cellColors.TryGetValue(1, out palette);
     }
-
 
     void CreateColorPallette(int cellType, Vector4 baseColor)
     {
-        Color32[] colorPallette = new Color32[paletteSize];
+        uint[] colorPallette = new uint[paletteSize];
 
         for (int i = 0; i < paletteSize; i++)
         {
             // Randomize color.xyzw from 0 to 1 then multiply baseColor.x then normalize
-            float x = Random.Range(0f, 1f) * baseColor.x;
-            float y = Random.Range(0f, 1f) * baseColor.y;
-            float z = Random.Range(0f, 1f) * baseColor.z;
-            float w = 1f;
-            Vector4 colorV4 = new Vector4(x, y, z, w);
+            float r = Random.Range(0f, 1f) * baseColor.x;
+            //Debug.Log("r :"+r);
+            float g = Random.Range(0f, 1f) * baseColor.y;
+            float b = Random.Range(0f, 1f) * baseColor.z;
+            float a = 1f;
+            Vector4 colorV4 = new Vector4(r, g, b, a);
+            //Debug.Log("colorV4 :" + colorV4);
             colorV4.Normalize();
-            colorPallette[i] = Vector4ToColor32(colorV4);
+            //colorPallette[i] = Vector4ToColor32(colorV4);
+            uint color = Vector4ToColor32(colorV4);
+            Debug.Log("color :" + color);
         }
 
         cellColors.Add(cellType, colorPallette);
     }
+
+    public uint Vector4ToColor32(Vector4 unpackedColor)
+    {
+        uint packedColor;
+
+        unpackedColor.x = Mathf.Min(unpackedColor.x, 0x000000ff);
+        unpackedColor.y = Mathf.Min(unpackedColor.y, 0x000000ff);
+        unpackedColor.z = Mathf.Min(unpackedColor.z, 0x000000ff);
+        unpackedColor.w = Mathf.Min(unpackedColor.w, 0x000000ff);
+
+        packedColor = (uint)((uint)(unpackedColor.x*255) |
+                            ((uint)(unpackedColor.y*255) << 8) |
+                            ((uint)(unpackedColor.z*255) << 16) |
+                            ((uint)(unpackedColor.w*255) << 24));
+
+        return packedColor;
+    }
+
+    
+
 }
